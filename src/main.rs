26358a5a5afa7992ca7c626d594377a34b8714e9
han chiang <yaphc@hotmail.com>
2022-03-12 16:57:46 +0800

    Handle concurrent clients

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 #[allow(unused_imports)]
 use std::env;
+use std::error::Error;
 #[allow(unused_imports)]
 use std::fs;
 use std::io::{Read, Write};
 #[allow(unused_imports)]
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
+use std::thread;
+use tokio::io::AsyncReadExt;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -14,26 +17,26 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
-        match stream {
-            Ok(mut s) => {
-                let mut buffer: [u8; 1024] = [0; 1024];
-
-                loop {
-                    match s.read(&mut buffer) {
-                        Ok(_size) => {
-                            let res = format_response("PONG");
-                            s.write(res.as_bytes());
-                        },
-                        Err(e) => {
-                            println!("Error reading from stream: {:?}", e);
-                            break;
-                        }
-                    }
-                }
-            },
-            Err(e) => println!("Couldn't accept client: {:?}", e)
-        }
+        handle_connection(stream.unwrap());
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    thread::spawn(move || {
+        let mut buffer: [u8; 1024] = [0; 1024];
+        loop {
+            match stream.read(&mut buffer) {
+                Ok(_size) => {
+                    let res = format_response("PONG");
+                    stream.write(res.as_bytes());
+                }
+                Err(e) => {
+                    println!("Error reading from stream: {:?}", e);
+                    break;
+                }
+            }
+        }
+    });
 }
 
 fn format_response(res: &str) -> String {
